ed183ed36e3e3e887285bbe23e730395260e26d7
Tomasz Wawrzyniak <tomasz.wawrzyniak@capgemini.com>
2024-10-17 15:00:42 +0200

    HttpClient

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,11 +6,13 @@ import {
   withDebugTracing,
 } from '@angular/router';
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes,  withComponentInputBinding()),
+    provideHttpClient(),
     provideZoneChangeDetection({ eventCoalescing: true }),
   ],
 };
--- src/app/book/services/book.service.ts
+++ src/app/book/services/book.service.ts
@@ -1,60 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Book } from '../model';
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { BehaviorSubject, delay, filter, map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BookService {
-  private booksSubject = new BehaviorSubject<Book[]>([
-    {
-      id: 1,
-      author: 'F. Scott Fitzgerald',
-      title: 'The Great Gatsby',
-    },
-    {
-      id: 2,
-      author: 'J.D. Salinger',
-      title: 'The Catcher in the Rye',
-    },
-    {
-      id: 3,
-      author: 'George Orwell',
-      title: '1984',
-    },
-    {
-      id: 4,
-      author: 'Harper Lee',
-      title: 'To Kill a Mockingbird',
-    },
-  ]);
+  private readonly httpClient = inject(HttpClient);
 
-constructor(){
-  this.booksSubject.subscribe(console.log)
-}
+
   findAll(): Observable<Book[]> {
-    return this.booksSubject.asObservable();
+    // return this.booksSubject.asObservable();
+    return new Observable();
   }
 
   findOne(id: number): Observable<Book> {
-    return this.booksSubject.pipe(
-      map((arr)=> arr.filter(book=> book.id === id)[0]),
-    );
+    return this.httpClient.get<Book>(`http://localhost:3000/book/${id}`)
   }
 
   update(updatedBook: Book): Observable<Book> {
-    return new Observable<Book>((subscriber) => {
-      // setTimeout(() => {
-      const currentBooks = this.booksSubject.getValue();
-      const updatedBookCopy = { ...updatedBook };
-      const updatedBooks = currentBooks.map((book) =>
-        book.id === updatedBook.id ? updatedBookCopy : book
-      );
-      console.log("updated!!")
-      this.booksSubject.next(updatedBooks);
-      subscriber.next(updatedBookCopy);
-      subscriber.complete();
-      // }, 2000)
-    }).pipe(delay(2000));
+    return this.httpClient.put<Book>(`http://localhost:3000/book/${updatedBook.id}`, updatedBook);
   }
 }
